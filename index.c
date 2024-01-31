1e0a1c92e6a607a78293cb199655e34f28c215f2
Isa Carvalho <isadecarvalhodev@gmail.com>
2024-01-31 09:37:33 +0000

    first commit

---
+++ index.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+int main() {
+
+printf("Length:  ");
+
+int length;
+scanf("%d", &length);
+
+if (length <= 0) {
+    printf("Password length must be >= 1!");
+
+    return 1;
+
+
+
+
+}
+
+
+    return 0;
+}
