9ee3d8b6efc27a017ee8d9d3b82c79962a3efe21
Isa Carvalho <isadecarvalhodev@gmail.com>
2024-01-31 11:14:58 +0000

    index C

--- index.c
+++ index.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
 
@@ -10,7 +13,22 @@ scanf("%d", &length);
 if (length <= 0) {
     printf("Password length must be >= 1!");
 
+
     return 1;
+char *password = malloc(length + 1);
+
+free(password);
+
+char *digits = "0123456789";
+int digits_length = strlen(digits);
+
+char *lowers = "abcdefghijklmnpqrstuwyz";
+int lowers_length = strlen(lowers);
+int uppers_length = strlen(uppers);
+
+char *symbols = "!@#$%^&*()";
+
+int symbols_length = strlen(symbols);
 
 
 
